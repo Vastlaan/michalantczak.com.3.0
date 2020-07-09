d3a41b1c1786b62b1966f915cb8023aacb872286
vastlaan <vastlaan@mail.com>
2020-07-09 08:36:11 +0200

    update port

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4500;
 
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
