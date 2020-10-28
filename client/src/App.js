afa63ca731942a9f1c881dc0ebb1f14e15d19f0c
vastlaan <vastlaan@mail.com>
2020-10-28 11:35:22 +0100

    Fetch payed heroku apps on start, to run them

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { IntlProvider } from "react-intl";
 import { Context } from "./store";
 import nl from "./translations/nl.json";
@@ -7,6 +7,15 @@ import Main from "./components/Main";
 
 function App() {
     const { store, dispatch } = useContext(Context);
+
+    useEffect(() => {
+        fetch("https://clairehempshop.herokuapp.com", {
+            mode: "no-cors",
+        }).catch((e) => console.error(e));
+        fetch("https://www.royalresumegenerator.com", {
+            mode: "no-cors",
+        }).catch((e) => console.error(e));
+    });
 
     return (
         <IntlProvider
